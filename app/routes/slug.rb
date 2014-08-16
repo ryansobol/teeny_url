e2e457c13c4221c84e7820371f7b5193cce5f1d1
Ryan Sobol <contact@ryansobol.com>
2014-08-15 22:38:32 -0700

    Remove the /:slug/info page

--- app/routes/slug.rb
+++ app/routes/slug.rb
@@ -1,20 +1,16 @@
-get '/:slug/info' do |slug|
-  @link = Link.find_by(slug: slug)
-  redirect '/' if @link.nil?
-
-  erb :info
-end
-
 get '/:slug' do |slug|
   link = Link.find_by(slug: slug)
+
   pass if link.nil?
 
   link.inc(count: 1)
+
   redirect link.url
 end
 
 delete '/:slug' do |slug|
   link = Link.find_by(slug: slug)
+
   link.destroy if link
 
   redirect '/'
