821ab8cd525b7deef37d7f550ec4a79335675aa0
Ryan Sobol <contact@ryansobol.com>
2015-01-14 06:32:40 -0800

    Rename routes to controllers

--- teeny_url.rb
+++ teeny_url.rb
@@ -2,9 +2,9 @@ require 'bundler'
 Bundler.require
 
 require_relative 'app/models/link'
-require_relative 'app/routes/404'
-require_relative 'app/routes/root'
-require_relative 'app/routes/slug'
+require_relative 'app/controllers/404'
+require_relative 'app/controllers/root'
+require_relative 'app/controllers/slug'
 
 set :root, File.join(settings.root, 'app')
 
